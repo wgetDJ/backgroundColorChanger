2cdd4e91e4ad9f88132f03c3da8a9ebf203ec1e1
wgetdj <dibyajyotimishraiiit@gmail.com>
2021-05-30 15:23:37 +0530

    Adding copy to clipboard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,15 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
 
 const btn = document.getElementById("btn");
 const colour = document.getElementById("colour");
+const copy = document.getElementById("copy");
 
-btn.addEventListener("click", function(){
-    document.body.style.backgroundColor = hexColour();
-    colour.textContent = hexColour();
-});
+const colourChange = () => {
+    let newColour = hexColour();
+    document.body.style.backgroundColor = newColour;
+    colour.textContent = newColour;
+    colour.style.color = newColour;
+    copy.style.color = "#eeeeee"; // Resetting the copy color
+}
 
 const hexColour = () => {
     let hexValue = "#";
@@ -15,3 +19,13 @@ const hexColour = () => {
     }
     return hexValue;
 }
+
+const copyColour = () => {
+    let colourToCopy = document.getElementById("colour");
+    let cb = navigator.clipboard;
+    cb.writeText(colourToCopy.innerText);
+    copy.style.color = "#15e638";
+}
+
+btn.addEventListener("click", colourChange);
+copy.addEventListener("click", copyColour);
